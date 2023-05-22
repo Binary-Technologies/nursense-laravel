f8330820ed19ae4ccb7a200487c96a46f7690c9d
Devmin <devmin_222@hotmail.com>
2023-05-22 17:02:31 +0530

    Add DB seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,52 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('news')->insert([
+            'title' => '2023년도 하반기 교육과정 업데이트 안내',
+            'content' => '안녕하세요, 디지털스마트교육센터입니다. 디지털스마트교육센터 상반기 교육과정 업데이트가 2023년 5월 29일(월)에 진행됩니다. 자세한 업데이트 사항은 아래 내용을 확인해 주시기 바랍니다.',
+            'main_exposure' => 1,
+            'exposure' => 0,
+            'views' => 1,
+        ]);
+
+        DB::table('news')->insert([
+            'title' => '동영상 재생 안 될 경우 조치 사항',
+            'content' => '동영상 재생 안 될 경우 조치 사항',
+            'main_exposure' => 0,
+            'exposure' => 1,
+            'views' => 0,
+        ]);
+
+        DB::table('news')->insert([
+            'title' => '디지털스마트교육센터 모바일 학습을 위한 설정 매뉴얼 안내',
+            'content' => '디지털스마트교육센터 모바일 학습을 위한 설정 매뉴얼 안내',
+            'main_exposure' => 0,
+            'exposure' => 1,
+            'views' => 0,
+        ]);
+
+        DB::table('notices')->insert([
+            'title' => '디지털스마트교육센터 2023년도 상반기 우수사업 선정',
+            'content' => '안녕하세요 디지털스마트교육센터입니다. 디지털스마트교육센터가 2023년도 우수사업에 선정되었습니다. 디지털스마트교육센터는 보건간호교육의 핵심역량을 강화하고 새로운 패러다임을 확보하고자 하는 디지털 교육 플랫폼입니다.',
+            'main_exposure' => 1,
+            'exposure' => 0,
+            'views' => 1,
+        ]);
+
+        DB::table('notices')->insert([
+            'title' => '2023년 디지털스마트교육센터 홍보 영상',
+            'content' => '2023년 디지털스마트교육센터 홍보 영상',
+            'main_exposure' => 0,
+            'exposure' => 1,
+            'views' => 0,
+        ]);
+
+        DB::table('notices')->insert([
+            'title' => '고령층 스마트폰 활용 교육 우수사례 소개',
+            'content' => '고령층 스마트폰 활용 교육 우수사례 소개',
+            'main_exposure' => 0,
+            'exposure' => 1,
+            'views' => 0,
+        ]);
     }
 }
---
+++ database/seeders/NewsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('news')->insert([
+            'title' => '2023년도 하반기 교육과정 업데이트 안내',
+            'content' => '안녕하세요, 디지털스마트교육센터입니다. 디지털스마트교육센터 상반기 교육과정 업데이트가 2023년 5월 29일(월)에 진행됩니다. 자세한 업데이트 사항은 아래 내용을 확인해 주시기 바랍니다.',
+            'main_exposure' => 1,
+            'exposure' => 0,
+            'views' => 1,
+        ],
+        [
+            'title' => '2023년도 하반기 교육과정 업데이트 안내',
+            'content' => '안녕하세요, 디지털스마트교육센터입니다. 디지털스마트교육센터 상반기 교육과정 업데이트가 2023년 5월 29일(월)에 진행됩니다. 자세한 업데이트 사항은 아래 내용을 확인해 주시기 바랍니다.',
+            'main_exposure' => 1,
+            'exposure' => 0,
+            'views' => 1,
+        ]);
+    }
+}
