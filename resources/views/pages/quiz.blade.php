a7aacf9c91c12e2e8f2adae5c64c6cf528248301
Devmin <devmin_222@hotmail.com>
2023-06-26 19:57:08 +0530

    fix quiz alignment

--- resources/views/pages/quiz.blade.php
+++ resources/views/pages/quiz.blade.php
@@ -3,13 +3,10 @@
 @section('content')
 <section class="pt100 pb100">
     <div class="row">
-
         <div class="col-lg-2">
             @include('includes.sidenav-info')
         </div>
-
         <div class="col-lg-10 bg-quiz" style="width: 100%; padding: 0px;">
-
             <section class="img-sec-top text-center comp-height-120 py44 pl0 pr0" style="background-image: linear-gradient(to right, #212880 , #212880);">
                 <div class="pl30">
                     <div class="content-box">
@@ -19,105 +16,98 @@
                     </div>
                 </div>
             </section>
-
             <div class="my-5">
+                <!-- Top card start -->
                 <div class="row item-flex-center">
-
-                    <!-- Top card start -->
-
-                        <div class="col-lg-9 shadow border-t-custom-quiz border-rad-10 px-4 mb-3 bg-quiz2">
-                            <div class="row py-4">
-                                <div class="col-md-12 item-flex-left curr-in-box">
-                                    <p class="ttl-18">{{ $quiz->name }}</p>
-                                </div>
-                                <div class="col-md-12 item-flex-left curr-in-box">
-                                    <p class="ttl-16 text-justify">{{ $quiz->description }}</p>
-                                </div>
-                                <div class="col-md-12 item-flex-left curr-in-box pt-3">
-                                    <span class="ttl-27 pr-4 relative-block border-r2">총 문제 수</span>
-                                    <span class="ttl-28">{{ count($quiz->questions) }}개</span>
-                                </div>
-                                <div class="col-md-12 item-flex-left curr-in-box pt-2">
-                                    <span class="ttl-27">종료일시</span>
-                                    <span class="ttl-28 pr-quiz-date">{{ $quiz->deadline_date }}</span>
-                                </div>
+                    <div class="col-lg-9 shadow border-t-custom-quiz border-rad-10 px-4 mb-3 bg-quiz2">
+                        <div class="row py-4">
+                            <div class="col-md-12 item-flex-left curr-in-box">
+                                <p class="ttl-18">{{ $quiz->name }}</p>
+                            </div>
+                            <div class="col-md-12 item-flex-left curr-in-box">
+                                <p class="ttl-16 text-justify">{{ $quiz->description }}</p>
+                            </div>
+                            <div class="col-md-12 item-flex-left curr-in-box pt-3">
+                                <span class="ttl-27 pr-4 relative-block border-r2">총 문제 수</span>
+                                <span class="ttl-28">{{ count($quiz->questions) }}개</span>
+                            </div>
+                            <div class="col-md-12 item-flex-left curr-in-box pt-2">
+                                <span class="ttl-27">종료일시</span>
+                                <span class="ttl-28 pr-quiz-date">{{ $quiz->deadline_date }}</span>
                             </div>
                         </div>
                     </div>
-                    <form action="/curriculum/quiz" method="POST" id="quiz-form">
-                    @csrf
-                        <input type="hidden" name="pre_learning_id" value="{{ $quiz->id }}">
-                        <!-- Top card end -->
-                        <?php $i = 1; ?>
-                        @foreach ($quiz->questions as $question)
-                        <!-- card -->
-                        <div class="col-lg-9 shadow border-rad-10 px-4 mb-3 bg-quiz2">
-                            <div class="row border-b-cus pt-2">
-                                <div class="col-md-12 position-rel curr-in-box py-2">
-                                    <div class="row my-2">
-                                        <div class="col-md-9 item-flex-left">
-                                            <p class="ttl-12-2 mb-0">
-                                                {{ $i.'. '.$question->question }}
-                                            </p>
-                                        </div>
-                                        <div class="col-md-3 item-flex-right">
-                                            <p class="ttl-19 mb-0 mt--16">
-                                                {{ $question->points }}점
-                                            </p>
-                                        </div>
+                </div>
+                <!-- Top card end -->
+                <form action="/curriculum/quiz" method="POST" id="quiz-form">
+                @csrf
+                    <input type="hidden" name="pre_learning_id" value="{{ $quiz->id }}">
+                    <?php $i = 1; ?>
+                    @foreach ($quiz->questions as $question)
+                    <!-- card -->
+                    <div class="col-lg-9 shadow border-rad-10 px-4 mb-3 bg-quiz2">
+                        <div class="row border-b-cus pt-2">
+                            <div class="col-md-12 position-rel curr-in-box py-2">
+                                <div class="row my-2">
+                                    <div class="col-md-9 item-flex-left">
+                                        <p class="ttl-12-2 mb-0">
+                                            {{ $i.'. '.$question->question }}
+                                        </p>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="row py-4">
-                                <div class="col-md-8 position-rel">
-                                    <div class="form-check mb-3">
-                                        <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-1-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_1 !!}')">
-                                        <label class="form-check-label" for="exampleRadios1{{ $i }}">
-                                            {{ $question->option_1 }}
-                                        </label>
-                                    </div>
-                                    <div class="form-check mb-3">
-                                        <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-2-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_2 !!}')">
-                                        <label class="form-check-label" for="exampleRadios2{{ $i }}">
-                                            {{ $question->option_2 }}
-                                        </label>
-                                    </div>
-                                    <div class="form-check mb-3">
-                                        <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-3-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_3 !!}')">
-                                        <label class="form-check-label" for="exampleRadios3{{ $i }}">
-                                            {{ $question->option_3 }}
-                                        </label>
-                                    </div>
-                                    <div class="form-check mb-3">
-                                        <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-4-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_4 !!}')">
-                                        <label class="form-check-label" for="exampleRadios4{{ $i }}">
-                                            {{ $question->option_4 }}
-                                        </label>
-                                    </div>
-                                    <div class="form-check mb-3">
-                                        <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-5-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_5 !!}')">
-                                        <label class="form-check-label" for="exampleRadios5{{ $i }}">
-                                            {{ $question->option_5 }}
-                                        </label>
+                                    <div class="col-md-3 item-flex-right">
+                                        <p class="ttl-19 mb-0 mt--16">
+                                            {{ $question->points }}점
+                                        </p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- card -->
-                        <?php $i++; ?>
-                        @endforeach
-
-                        {{-- <div class="col-md-12 item-flex-center pt-5 pb-4">
-                            <button class="btn btn-outline-secondary btn-sm btn-curr" type="submit" style="color: #ffffff; background-color: #212880;padding-left: 40px;padding-right: 40px;padding-bottom: 10px;padding-top: 10px;">제출</button>
-                        </div> --}}
-                    </form>
+                        <div class="row py-4">
+                            <div class="col-md-8 position-rel">
+                                <div class="form-check mb-3">
+                                    <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-1-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_1 !!}')">
+                                    <label class="form-check-label" for="exampleRadios1{{ $i }}">
+                                        {{ $question->option_1 }}
+                                    </label>
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-2-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_2 !!}')">
+                                    <label class="form-check-label" for="exampleRadios2{{ $i }}">
+                                        {{ $question->option_2 }}
+                                    </label>
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-3-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_3 !!}')">
+                                    <label class="form-check-label" for="exampleRadios3{{ $i }}">
+                                        {{ $question->option_3 }}
+                                    </label>
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-4-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_4 !!}')">
+                                    <label class="form-check-label" for="exampleRadios4{{ $i }}">
+                                        {{ $question->option_4 }}
+                                    </label>
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input class="form-check-input" type="radio" name="preLearningAnswers[{{ $question->id }}]" id="exampleRadios-5-{{ $i }}" value="" onchange="updateValue(this, '{!! $question->option_5 !!}')">
+                                    <label class="form-check-label" for="exampleRadios5{{ $i }}">
+                                        {{ $question->option_5 }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <!-- card -->
+                    <?php $i++; ?>
+                    @endforeach
 
-                    <div class="col-md-12 item-flex-center pt-5 pb-4">
-                        <a href="#scoreModal" class="btn btn-2" data-bs-toggle="modal">제출</a>
-                    </div>
-
+                    {{-- <div class="col-md-12 item-flex-center pt-5 pb-4">
+                        <button class="btn btn-outline-secondary btn-sm btn-curr" type="submit" style="color: #ffffff; background-color: #212880;padding-left: 40px;padding-right: 40px;padding-bottom: 10px;padding-top: 10px;">제출</button>
+                    </div> --}}
+                <!-- card -->
+                </form>
+                <div class="col-md-12 item-flex-center pt-5 pb-4">
+                    <a href="#scoreModal" class="btn btn-2" data-bs-toggle="modal">제출</a>
                 </div>
             </div>
 
@@ -146,12 +136,9 @@
                 </div>
             </div>
             <!-- Score Alert Modal -->
-
         </div>
     </div>
 </section>
-
-
 @endsection
 
 @section('scripts')
