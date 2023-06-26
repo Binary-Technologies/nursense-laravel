0c65f76fa77770a7c10b0a93b7e0cabb0e22dc66
Devmin <devmin_222@hotmail.com>
2023-06-26 12:48:37 +0530

    Update news seeder

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -20,13 +20,17 @@ class NewsSeeder extends Seeder
             'main_exposure' => 1,
             'exposure' => 0,
             'views' => 1,
+            'created_at' => '2023-06-05 01:36:00',
+            'updated_at' => '2023-06-05 01:36:00',
         ],
         [
             'title' => '2023년도 하반기 교육과정 업데이트 안내',
             'content' => '안녕하세요, 디지털스마트교육센터입니다. 디지털스마트교육센터 상반기 교육과정 업데이트가 2023년 5월 29일(월)에 진행됩니다. 자세한 업데이트 사항은 아래 내용을 확인해 주시기 바랍니다.',
-            'main_exposure' => 1,
+            'main_exposure' => 0,
             'exposure' => 0,
             'views' => 1,
+            'created_at' => '2023-06-05 01:36:00',
+            'updated_at' => '2023-06-05 01:36:00',
         ]);
     }
 }
